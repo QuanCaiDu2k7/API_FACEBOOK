27e87f0b372605de65f23c9bc6b9d4cbd79f480d
QuanCaiDu2k7 <88080637+QuanCaiDu2k7@users.noreply.github.com>
2022-05-30 23:08:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from typing import Optional
-
 from fastapi import FastAPI
-
 import requests
-
 app = FastAPI()
 
 def get_token(cookies):
@@ -27,8 +24,23 @@ def get_token(cookies):
         access_ = get.split('#access_token=')[1].split('&data_access')[0]
         return access_
     except:
-        return False
-@app.get("/get-token")
+        return 'Không Thể Get Token!'
+
+def like_ig(id_post, cookies):
+    try:
+        token=cookies.split('csrftoken=')[1].split(';')[0]
+        get_id = requests.post('https://www.instagram.com/p/'+id_post+'/',headers={'Authority': 'www.instagram.com','Content-Length': '0','Sec-Ch-Ua': '\"Google Chrome\";v=\"93\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"93\"','X-Ig-App-Id': '1217981644879628','Sec-Ch-Ua-Mobile': '?1','X-Instagram-Ajax': 'd26a8ffbcd2b','Content-Type': 'application/x-www-form-urlencoded','Accept': '*/*','X-Requested-With': 'XMLHttpRequest','X-Asbd-Id': '198387','X-Csrftoken': token,'Sec-Ch-Ua-Platform': '\"Android\"','Origin': 'https://www.instagram.com','Sec-Fetch-Site': 'same-origin','Sec-Fetch-Mode': 'cors','Sec-Fetch-Dest': 'empty','Referer': 'https://www.instagram.com/','Accept-Language': 'en-US,en;q=0.9,vi;q=0.8','Cookie': cookies}).text
+        id_lam = get_id.split('content="instagram://media?id=')[1].split('"/>\n<meta')[0]
+        like = requests.post('https://www.instagram.com/web/likes/'+str(id_lam)+'/like/',headers={'Authority': 'www.instagram.com','Content-Length': '0','Sec-Ch-Ua': '\"Google Chrome\";v=\"93\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"93\"','X-Ig-App-Id': '1217981644879628','Sec-Ch-Ua-Mobile': '?1','X-Instagram-Ajax': 'd26a8ffbcd2b','Content-Type': 'application/x-www-form-urlencoded','Accept': '*/*','X-Requested-With': 'XMLHttpRequest','X-Asbd-Id': '198387','X-Csrftoken': token,'Sec-Ch-Ua-Platform': '\"Android\"','Origin': 'https://www.instagram.com','Sec-Fetch-Site': 'same-origin','Sec-Fetch-Mode': 'cors','Sec-Fetch-Dest': 'empty','Referer': 'https://www.instagram.com/','Accept-Language': 'en-US,en;q=0.9,vi;q=0.8','Cookie': cookies,}).json()
+        return {'id': id_lam, 'msg': like}
+    except:
+        return {id_post: 'Thất Bại'}
+@app.get("/get_token")
 def read_item(cookies: Optional[str] = None):
     access_token = get_token(cookies)
     return {"access_token": access_token}
+
+@app.post("/like_ig/{id_post}")
+def read_item(id_post: str, cookies: Optional[str] = None):
+    like = like_ig(id_post, cookies)
+    return like
