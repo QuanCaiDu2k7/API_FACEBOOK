6151e0c8da1c26ddcd9d3ddb7f44d78eca833f25
QuanCaiDu2k7 <88080637+QuanCaiDu2k7@users.noreply.github.com>
2022-06-01 03:19:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ headers = {
 }
 
 def tiki(phone):
+    if phone == "0395524153" and "84395524153":
+        return "Spam Số Tao Làm Lồn À!"
+    else:
         try:
             json_data = {
                     'phone_number': phone,
@@ -28,83 +31,78 @@ def tiki(phone):
             return "Lỗi Không Xác Định!"
 
 def grab_food(phone):
-    try:
-        json_data = {
-            'client_id': random_id(32),
-            'ctx_id': random_id(32),
-            'transaction_ctx': None,
-            'country_code': 'VN',
-            'method': 'SMS',
-            'num_digits': 6,
-            'scope': 'openid profile.read foodweb.order foodweb.rewards foodweb.get_enterprise_profile',
-            'phone_number': phone,
-        }
-        response_grab_food = requests.post('https://partner-api.grab.com/grabid/v1/oauth2/otp', headers=headers, json=json_data).text
-        return response_grab_food
-    except:
-        return "Lỗi Không Xác Định!"
-    
-def bach_hoa_xanh(phone):
-    try:
-        data = {
-            'phone': phone,
-            'objectId': random_id(36),
-            'type': '4',
-        }
-        response_bach_hoa_xanh = requests.post('https://www.bachhoaxanh.com/aj/Customer/SendOTP', headers=headers, data=data).text
-        return response_bach_hoa_xanh
-    except:
-        return "Lỗi Không Xác Định!" 
-    
-def meta_vn(phone):
-    try:
-        params = {
-            'api_mode': '1',
-        }
-
-        json_data = {
-            'api_args': {
-                'lgUser': phone,
-                'act': 'send',
-                'type': 'phone',
-            },
-            'api_method': 'CheckExist',
-        }
-
-        response_meta_vn = requests.post('https://meta.vn/app_scripts/pages/AccountReact.aspx', params=params, headers=headers, json=json_data).text
-        return response_meta_vn
-    except:
-        return "Lỗi Không Xác Định!"    
-    
-
-@app.post("/tiki")
-def read_item(phone: Optional[str] = None):
-    if phone == "0395524153" or "84395524153":
+    if phone == "0395524153" and "84395524153":
         return "Spam Số Tao Làm Lồn À!"
     else:
-        done = tiki(phone)
-        return done
+        try:
+            json_data = {
+                'client_id': random_id(32),
+                'ctx_id': random_id(32),
+                'transaction_ctx': None,
+                'country_code': 'VN',
+                'method': 'SMS',
+                'num_digits': 6,
+                'scope': 'openid profile.read foodweb.order foodweb.rewards foodweb.get_enterprise_profile',
+                'phone_number': phone,
+            }
+            response_grab_food = requests.post('https://partner-api.grab.com/grabid/v1/oauth2/otp', headers=headers, json=json_data).text
+            return response_grab_food
+        except:
+            return "Lỗi Không Xác Định!"
+    
+def bach_hoa_xanh(phone):
+    if phone == "0395524153" and "84395524153":
+        return "Spam Số Tao Làm Lồn À!"
+    else:
+        try:
+            data = {
+                'phone': phone,
+                'objectId': random_id(36),
+                'type': '4',
+            }
+            response_bach_hoa_xanh = requests.post('https://www.bachhoaxanh.com/aj/Customer/SendOTP', headers=headers, data=data).text
+            return response_bach_hoa_xanh
+        except:
+            return "Lỗi Không Xác Định!" 
+    
+def meta_vn(phone):
+    if phone == "0395524153" and "84395524153":
+        return "Spam Số Tao Làm Lồn À!"
+    else:
+        try:
+            params = {
+                'api_mode': '1',
+            }
+
+            json_data = {
+                'api_args': {
+                    'lgUser': phone,
+                    'act': 'send',
+                    'type': 'phone',
+                },
+                'api_method': 'CheckExist',
+            }
+
+            response_meta_vn = requests.post('https://meta.vn/app_scripts/pages/AccountReact.aspx', params=params, headers=headers, json=json_data).text
+            return response_meta_vn
+        except:
+            return "Lỗi Không Xác Định!"    
+@app.post("/tiki")
+def read_item(phone: Optional[str] = None):
+    done = tiki(phone)
+    return done
 
 @app.post("/grab-food")
 def read_item(phone: Optional[str] = None):
-    if phone == "0395524153" or "84395524153":
-        return "Spam Số Tao Làm Lồn À!"
-    else:
-        done = grab_food(phone)
-        return done
+    done = grab_food(phone)
+    return done
 
 @app.post("/bach-hoa-xanh")
 def read_item(phone: Optional[str] = None):
-    if phone == "0395524153" or "84395524153":
-        return "Spam Số Tao Làm Lồn À!"
-    else:
-        done = bach_hoa_xanh(phone)
-        return done
+    done = bach_hoa_xanh(phone)
+    return done
 
 @app.post("/meta-vn")
 def read_item(phone: Optional[str] = None):
-    if phone == "0395524153" or "84395524153":
-        return "Spam Số Tao Làm Lồn À!"
-    else:
-        done = meta_vn(phone)
-        return done
+    done = meta_vn(phone)
+    return done
