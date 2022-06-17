8446f2f942cacf0197de6d0ee848c8b6f5c1d852
QuanCaiDu2k7 <88080637+QuanCaiDu2k7@users.noreply.github.com>
2022-06-17 11:24:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ import random
 from fake_headers import Headers
 from typing import Optional
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
 app = FastAPI()
+
+
+@app.get("/pydantic", response_class=RedirectResponse, status_code=302)
+async def redirect_pydantic():
+    return "https://pydantic-docs.helpmanual.io/"
 
 def user_agent():
     headers = Headers(headers=True).generate()['User-Agent']
